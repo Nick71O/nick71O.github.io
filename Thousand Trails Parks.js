226835e73a6a626678bd32af0322e4a3c8f53e5b
Nick71O <151349064+Nick71O@users.noreply.github.com>
2024-04-30 10:28:42 -0400

    Changed redirect to get its arrical and departure dates from the IndexedDB

--- Thousand Trails Parks.js
+++ Thousand Trails Parks.js
@@ -14,24 +14,39 @@ function getTimestamp() {
   return timestamp;
 }
 
+async function openTabs(arrivalDate, departureDate) {
+    arrivalDate = arrivalDate.replace(/\//g, "%2F");
+    departureDate = departureDate.replace(/\//g, "%2F");
+    var loginURL = baseURL + "/login/index"
+    var bookingQueryString = "?locationid=78&arrivaldate=" + arrivalDate + "&departuredate=" + departureDate + "&adults=2&children=3&pets=0&autos=0&category=1&equiptype=3&length=27"
+    var bookingURL = baseURL + "/reserve/startbooking" + bookingQueryString
+
+    console.log("Redirecting to the Campgrounds Booking Page");
+    console.log(bookingURL);
+    await sleep(500);
+    window.location.replace(bookingURL);
+}
+
 async function launch() { 
     getTimestamp();
-    var sharedBookingURL = localStorage.getItem("bookingQueryString")
-    //var sharedBookingURL = sessionStorage.getItem("bookingQueryString")
-    
-    if (sharedBookingURL != "") {      
-        console.log("Redirecting to the Campgrounds Booking Page");
-        console.log("sharedBookingURL: " + sharedBookingURL);
-        var bookingURL = baseURL + "/reserve/startbooking?" + sharedBookingURL
-        console.log(bookingURL);
-        await sleep(500);
-        window.location.replace(bookingURL);
-    }
-    else {
-        console.log("FAILED to redirect to the Campgrounds Booking Page. sharedBookingURL was blank");
-    }
+    try {
+        const db = await initializeDB();
+        console.log('IndexedDB initialized successfully.');
+        await logSiteConstants(db);
 
-    //window.location.replace("https://members.thousandtrails.com/reserve/startbooking?locationid=78&arrivaldate=07%2F06%2F2023&departuredate=07%2F10%2F2023&adults=2&children=3&pets=0&autos=0&category=1&equiptype=3&length=27");
+        const siteConstants = await getSiteConstants(db);
+        const scDesiredArrivalDate = siteConstants.DesiredArrivalDate;
+        const scDesiredDepartureDate = siteConstants.DesiredDepartureDate;
+        console.log("SiteConstants Desired Dates to Book\n   Arrival: " + scDesiredArrivalDate + "    Departure: " + scDesiredDepartureDate);
+
+        openTabs(scDesiredArrivalDate, scDesiredDepartureDate);
+
+    } catch (error) {
+        console.error('FAILED to redirect to the Campgrounds Booking Page. siteConstants in the IndexedDB was not loaded or blank', error);
+        await sleep(5000);
+        console.log("Reloading Page");
+        window.location.reload();
+    }
 }
 
 launch();
