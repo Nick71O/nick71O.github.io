e87d9b3d35db0f2ef649c20fd84294d5cfce37df
Nick71O <151349064+Nick71O@users.noreply.github.com>
2024-05-05 11:21:59 -0400

    Added a function to relogin

---
+++ Thousand Trails Common.js
@@ -0,0 +1,10 @@
+
+
+function getTimestamp() {
+    var nowDate = new Date();
+    var date = nowDate.toDateString();
+    var time = nowDate.toLocaleTimeString();
+    var timestamp = '--' + date + ', ' + time + '--';
+    console.log(timestamp);
+    return timestamp;
+}
