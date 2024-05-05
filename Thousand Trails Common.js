f224e70ec5155bc187171105a590032467b15928
Nick71O <151349064+Nick71O@users.noreply.github.com>
2024-05-05 12:18:38 -0400

    a

--- Thousand Trails Common.js
+++
@@ -1,8 +0,0 @@
-function logTimestamp() {
-    var nowDate = new Date();
-    var date = nowDate.toDateString();
-    var time = nowDate.toLocaleTimeString();
-    var timestamp = '--' + date + ', ' + time + '--';
-    console.log(timestamp);
-    return timestamp;
-}
