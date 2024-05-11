8c0007703ca15738df22bb32dbdb9f3e8bcd5edc
Nick71O <151349064+Nick71O@users.noreply.github.com>
2024-05-11 00:18:23 -0400

    moving stuff around

--- Thousand Trails Booking Reservation Details.js
+++ Thousand Trails Booking Reservation Details.js
@@ -486,25 +486,6 @@ async function AvailableBooking(db, availableDates, arrivalDate, departureDate, 
     return { availableArrivalDate, availableDepartureDate };
 }
 
-// Function to send a message using Pushover API
-async function sendMessage(userKey, apiToken, pushoverUrl, message, sound = '') {
-    try {
-        // Message data to send
-        const messageData = {
-            token: apiToken,
-            user: userKey,
-            message: message,
-            sound: sound,
-        };
-
-        // Send a POST request to Pushover API using Axios
-        const response = await axios.post(pushoverUrl, messageData);
-        console.log('Message sent:', response.data);
-    } catch (error) {
-        console.error('Error sending message:', error.response.data || error.message);
-    }
-}
-
 function concatenateAvailableDatesToString(datesArray) {
     let concatenatedString = 'Available Dates: ';
     if (datesArray.length === 0) {
--- Thousand Trails Common.js
+++ Thousand Trails Common.js
@@ -1,7 +1,66 @@
 console.log('Hello From Thousand Trails Common.js');
 
 // dynamically load additional scripts
-//const indexedDBScript = document.createElement('script');
-//indexedDBScript.src = 'https://nick71o.github.io/Thousand%20Trails%20IndexedDB.js';
-//document.head.appendChild(indexedDBScript);
+loadScript('https://nick71o.github.io/Thousand%20Trails%20IndexedDB.js')
+    /*
+    .then(() => {
+        // IndexedDB script has been successfully loaded
+        return loadScript('https://nick71o.github.io/Thousand%20Trails%20Common.js');
+    })
+    */
+    .then(() => {
+        // Common script has been successfully loaded
+        return loadScript('https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js');
+    })
+    .then(() => {
+        // Axios script has been successfully loaded
+        // Now you can safely use functions or variables from the loaded scripts here
+    })
+    .catch(error => {
+        // Handle errors if any script fails to load
+        console.error('Error loading scripts:', error);
+    });
 
+
+
+function loadScript(src) {
+    return new Promise((resolve, reject) => {
+        const script = document.createElement('script');
+        script.src = src;
+        script.defer = true; // Add the defer attribute
+
+        script.onload = () => {
+            console.log(`Script loaded: ${src}`);
+            resolve(); // Resolve the promise when the script is loaded
+        };
+
+        script.onerror = () => {
+            console.error(`Error loading script: ${src}`);
+            reject(new Error(`Error loading script: ${src}`));
+        };
+
+        document.head.appendChild(script);
+    });
+}
+
+
+// Function to send a message using Pushover API
+async function sendMessage(userKey, apiToken, pushoverUrl, message, sound = '') {
+    try {
+        // Message data to send
+        const messageData = {
+            token: apiToken,
+            user: userKey,
+            message: message,
+            sound: sound,
+        };
+
+        // Send a POST request to Pushover API using Axios
+        const response = await axios.post(pushoverUrl, messageData);
+        console.log('Message sent:', response.data);
+    } catch (error) {
+        console.error('Error sending message:', error.response.data || error.message);
+    }
+}
+
+//priority
